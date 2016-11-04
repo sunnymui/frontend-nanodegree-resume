ed525471724fb5877e47415fe1ad9a6a84ff1fce
Sunny Mui <sunnycmui@gmail.com>
2016-11-03 17:14:57 -0700

    added work experperience html creation functions

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,7 +3,8 @@ var header = '#header';
 var skills = '#skills';
 var top_contact = '#topContacts';
 var footer_contact = '#footerContacts';
-var experience = '#workExperience';
+var work_exp = '#workExperience';
+var last_work = '.work-entry:last';
 var education_section = '#education';
 
 // data
@@ -17,11 +18,12 @@ var bio = {
     "location" : "San Jose, CA"
   },
   "biopic" : "https://sunnymui.github.io/one-page-portfolio/img/bw.png",
-  "welcomeMessage" : "Hi there!",
+  "welcomeMessage" : "I'm a San Jose, CA based designer, developer, and marketer who's worked in startups and in digital marketing.",
   "skills" : [
-    "awesomeness",
-    "being cool",
-    "developing"
+    "Digital Marketing",
+    "Design",
+    "Web Development",
+    "Conversion Optimization"
   ]
 };
 
@@ -136,8 +138,12 @@ var formatted_mobile = HTMLmobile.replace('%data%',bio.contact.mobile);
 var formatted_location = HTMLlocation.replace('%data%',bio.contact.location);
 
 // work experience
-var formatted_employer = HTMLworkEmployer.replace('%data%', work.employer);
-var formatted_position = HTMLworkTitle.replace('%data%', work.position);
+var formatted_employer;
+var formatted_title;
+var employer_and_title;
+var formatted_dates;
+var formatted_location;
+var formatted_description;
 
 // education
 var formatted_edu_name = HTMLschoolName.replace('%data%', education.schools[0].name);
@@ -146,27 +152,56 @@ var formatted_edu_name = HTMLschoolName.replace('%data%', education.schools[0].n
 var formatted_welcome_msg = HTMLwelcomeMsg.replace('%data%',bio.welcomeMessage);
 var formatted_skills ;
 
-// add the html elements to the page
+//
+// ADD HTML elements to the page
+//
 
-// add skills to page if the skills array isn't empty
+// header
+// add skills to page header if the skills array isn't empty
 if (bio.skills.length !== 0 ) {
   $(header).append(HTMLskillsStart);
   // loop through skills array to add each one as a list item
-  for (i=0; i < bio.skills.length; i++) {
+  for (var i=0; i < bio.skills.length; i++) {
     // format each skill then append it
     formatted_skills = HTMLskills.replace('%data%',bio.skills[i]);
     $(skills).append(formatted_skills);
   }
 }
-
-$(header).prepend(formatted_pic, formatted_name, formatted_role);
+$(header).prepend(formatted_pic,
+                  formatted_name,
+                  formatted_role);
+$(header).append(formatted_welcome_msg);
 $(top_contact).append(formatted_email,
                       formatted_github,
                       formatted_mobile,
                       formatted_location);
+
+
+// work experience
+// add the info to the experience section for each job in the work object
+for (var i in work.jobs) {
+  $(work_exp).append(HTMLworkStart);
+  // format the infowith the helper functions
+  // note that i in a for-in loop returns the index, not the value
+  // use forEach if your array is just values and not key-value pairs
+  if (work.jobs.hasOwnProperty(i)) {
+    formatted_employer = HTMLworkEmployer.replace('%data%', work.jobs[i].employer);
+    formatted_title = HTMLworkTitle.replace('%data%', work.jobs[i].title);
+    formatted_dates = HTMLworkDates.replace('%data%', work.jobs[i].dates);
+    formatted_location = HTMLworkLocation.replace('%data%', work.jobs[i].location);
+    formatted_description = HTMLworkDescription.replace('%data%', work.jobs[i].description);
+    // concatenate employer and title into one text string
+    employer_and_title = formatted_employer + formatted_title;
+    // append to the work entry
+    $(last_work).append(employer_and_title,
+                        formatted_dates,
+                        formatted_location,
+                        formatted_description);
+  }
+}
+
+// footer
 $(footer_contact).append(formatted_email,
-                      formatted_github,
-                      formatted_mobile,
-                      formatted_location);
-$(header).append(formatted_welcome_msg);
-$(experience).append(formatted_employer, formatted_position);
+                        formatted_github,
+                        formatted_mobile,
+                        formatted_location);
